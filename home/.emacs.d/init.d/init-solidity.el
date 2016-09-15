53defa9806f3ffa0bc11aab3956614ba25667aca
Matt Smith <matt.the.smith@gmail.com>
2016-09-15 14:27:52 -0700

    better indenting solidity

--- home/.emacs.d/init.d/init-solidity.el
+++ home/.emacs.d/init.d/init-solidity.el
@@ -2,15 +2,27 @@
 
 ;; solidity-mode
 
+(defun solidity-align-modifiers (langelem)
+  (let ((pos (cdr langelem)))
+    (if (string= (buffer-substring-no-properties pos (+ pos 8))
+            "function")
+        0
+      c-basic-offset)))
+
 (req-package solidity-mode
   :require flymake-solidity
   :mode "\\.sol\\'"
-  :init
+  :config
   (setq solidity-solc-path "/usr/local/bin/solc")
-  (add-hook 'after-change-major-mode-hook
-            (lambda() (if (equal major-mode 'solidity-mode)
-                          (progn (setq c-basic-offset 4)
-                                 (c-set-offset (quote statement-cont) 0 nil)))))
+  :init
+  (add-hook
+   'after-change-major-mode-hook
+   (lambda()
+     (if (equal major-mode 'solidity-mode)
+         (progn (setq c-basic-offset 4)
+                (c-set-offset 'statement-cont
+                              'solidity-align-modifiers)))))
+
   :bind ((:map solidity-mode-map ("M-j" . nil))
          (:map solidity-mode-map ("M-a" . nil))
          (:map solidity-mode-map ("C-M-j" . nil))))
